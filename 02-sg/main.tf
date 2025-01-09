313fe5643b7181c5c42479e22b8c4b0cb6aab2ea
MachaSreenath <sreenathshree9@gmail.com>
2025-01-09 18:44:16 +0530

    roboshop terraform changes

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -58,6 +58,16 @@ module "user" {
   # sg_ingress_rules = var.user_sg_ingress_rules
 }
 
+module "cart" {
+  source = "../../terraform-aws-security-group"
+  project_name = var.project_name
+  environment = var.environment
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  sg_description = "Security group for cart"
+  sg_name = "cart"
+  # sg_ingress_rules = var.cart_sg_ingress_rules
+}
+
 # mongodb accepting connections from catalogue instance 
 resource "aws_security_group_rule" "mongodb_catalogue" {
   source_security_group_id = module.catalogue.sg_id
