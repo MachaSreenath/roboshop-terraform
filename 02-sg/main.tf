62d032e4ca626b04dc2fb1589d728b63ed2b9479
MachaSreenath <sreenathshree9@gmail.com>
2025-01-09 18:29:47 +0530

    roboshop terraform changes

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -8,6 +8,16 @@ module "mongodb" {
   # sg_ingress_rules = var.mongodb_sg_ingress_rules
 }
 
+module "redis" {
+  source = "../../terraform-aws-security-group"
+  project_name = var.project_name
+  environment = var.environment
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  sg_description = "Security group for redis"
+  sg_name = "redis"
+  # sg_ingress_rules = var.redis_sg_ingress_rules
+}
+
 module "catalogue" {
   source = "../../terraform-aws-security-group"
   project_name = var.project_name
