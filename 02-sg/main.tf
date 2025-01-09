df493f4df9b5b3144ec25f723c90b55a596b383c
MachaSreenath <sreenathshree9@gmail.com>
2025-01-09 12:50:03 +0530

    roboshop terraform changes

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -3,7 +3,7 @@ module "mongodb" {
   project_name = var.project_name
   environment = var.environment
   vpc_id = data.aws_ssm_parameter.vpc_id.value
-  sg_description = "Security group for Mongodb"
+  sg_description = "Security group for mongodb"
   sg_name = "mongodb"
   # sg_ingress_rules = var.mongodb_sg_ingress_rules
 }
@@ -30,6 +30,7 @@ module "user" {
 
 # mongodb accepting connections from catalogue instance 
 resource "aws_security_group_rule" "mongodb_catalogue" {
+  source_security_group_id = module.mongodb.sg_id
   type = "ingress"
   from_port = 27017
   to_port = 27017
@@ -38,6 +39,7 @@ resource "aws_security_group_rule" "mongodb_catalogue" {
 }
 
 resource "aws_security_group_rule" "mongodb_user" {
+  source_security_group_id = module.mongodb.sg_id
   type = "ingress"
   from_port = 27017
   to_port = 27017
