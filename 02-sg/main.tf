e91d2b0b3445f1c06ecdbcee345d86c873dc371a
MachaSreenath <sreenathshree9@gmail.com>
2025-01-09 11:28:04 +0530

    roboshop terraform changes

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -5,4 +5,5 @@ module "mongodb" {
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   sg_description = "Security group for Mongodb"
   sg_name = "mongodb"
+  sg_ingress_rules = var.mongodb_sg_ingress_rules
 }
--- 02-sg/variables.tf
+++ 02-sg/variables.tf
@@ -17,3 +17,22 @@ variable "project_name" {
 variable "environment" {
   default = "dev"
 }
+
+variable "mongodb_sg_ingress_rules" {
+  default = [
+    {
+        description = "Allow port 80"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+        description = "Allow port 443"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
