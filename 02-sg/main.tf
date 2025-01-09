20868609d8a7d751dff8df7cbd262f7110af95d6
MachaSreenath <sreenathshree9@gmail.com>
2025-01-09 12:16:46 +0530

    roboshop terraform changes

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -5,5 +5,15 @@ module "mongodb" {
   vpc_id = data.aws_ssm_parameter.vpc_id.value
   sg_description = "Security group for Mongodb"
   sg_name = "mongodb"
-  sg_ingress_rules = var.mongodb_sg_ingress_rules
+  # sg_ingress_rules = var.mongodb_sg_ingress_rules
+}
+
+module "catalogue" {
+  source = "../../terraform-aws-security-group"
+  project_name = var.project_name
+  environment = var.environment
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+  sg_description = "Security group for catalogue"
+  sg_name = "catalogue"
+  # sg_ingress_rules = var.catalogue_sg_ingress_rules
 }
