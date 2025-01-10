09f89e47e8502105bc328a302e9502f2fbab2124
MachaSreenath <sreenathshree9@gmail.com>
2025-01-10 12:53:52 +0530

    roboshop terraform changes

--- 04-ec2/ec2.tf
+++ 04-ec2/ec2.tf
@@ -2,5 +2,6 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   name = "${local.ec2_name}-mongodb"
   instance_type = "t3.small"
-  vpc_security_group_ids = []
+  vpc_security_group_ids = [data.mongodb_sg_id.value]
+  
 }
